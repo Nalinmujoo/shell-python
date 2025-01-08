ef0cd6fb5a2c80f5d7f885cb6136027576fdcd9c
Nalinmujoo <nalinmujoo@gmail.com>
2025-01-08 09:41:01 -0500

    REPL

--- app/main.py
+++ app/main.py
@@ -5,10 +5,13 @@ def main():
     # Uncomment this block to pass the first stage
     sys.stdout.write("$ ")
 
+    commands = []
     # Wait for user input
     command = input()
 
-    print(f"{command}: command not found\n")
+    if command not in commands:
+        print(f"{command}: command not found\n")
+        main()
 
 
 if __name__ == "__main__":
