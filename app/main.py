f34f3479afd47e8c09e8438b08119d42393bb35e
Nalinmujoo <nalinmujoo@gmail.com>
2025-01-31 23:06:26 -0500

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,20 +1,32 @@
 import sys
 
-commands = set()
+commands = set(['exit'])
 
 def write(s):
     sys.stdout.write(s)
     sys.stdout.flush()
 
 def main():
-    write("$ ")
-    
-    # Wait for user input
-    command = input()
 
-    if command not in commands:
-        print(f"{command}: command not found")
-        main()
+    while True:
+        write("$ ")
+        
+        # Wait for user input
+        command = input()
+
+        # print("cmd", command)
+
+        args = command.split(" ")
+
+        # print("args", args)
+
+        if args[0] not in commands:
+            print(f"{command}: command not found")
+            continue
+
+        if args[0] == "exit":
+            if len(args) > 1 and args[1].isdigit():
+                sys.exit(int(args[1]))       
 
 
 if __name__ == "__main__":
