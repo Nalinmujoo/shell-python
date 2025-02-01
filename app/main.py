6f0714c71da7357666a0868e3e8334857e5ccc4f
Nalinmujoo <nalinmujoo@gmail.com>
2025-01-31 23:43:52 -0500

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,10 +1,17 @@
 import sys
+import os
 
 commands = set(['exit', 'echo', 'type'])
 
 def write(s):
     sys.stdout.write(s)
     sys.stdout.flush()
+
+def find_in_path(command):
+    for path in os.environ["PATH"].split(":"):
+        if os.path.exists(f"{path}/{command}"):
+            return f"{path}/{command}"
+    return None
 
 def main():
 
@@ -36,9 +43,11 @@ def main():
             else:
                 sys.stdout.write("\n")
 
-        if args[0] == "type":
-            if len(args) > 1 and args[1] in commands:
+        if args[0] == "type" and len(args) > 1:
+            if args[1] in commands:
                 sys.stdout.write(f"{args[1]} is a shell builtin\n")
+            elif find_in_path(args[1]):
+                sys.stdout.write(f"{args[1]} is {find_in_path(args[1])}\n")
             else:
                 sys.stdout.write(f"{args[1]}: not found\n")
 
